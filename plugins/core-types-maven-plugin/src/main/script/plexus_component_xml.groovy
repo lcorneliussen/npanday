464f78218edab5eeaa026c0bd7791b7c03c587b6
Lars Corneliussen <me@lcorneliussen.de>
2010-03-23 16:43:05 +0100

    Added 'legacy'-types to core-types and cleaned the components.xml

--- plugins/core-types-maven-plugin/src/main/script/plexus_component_xml.groovy
+++ plugins/core-types-maven-plugin/src/main/script/plexus_component_xml.groovy
@@ -1,10 +1,18 @@
 // define npanday core-types
+
+// TODO: Move to it's own file.
 List types = [
+  [name: 'dotnet-none', ext: 'none'],
+
   [name: 'dotnet-library', ext: 'dll'],
+  [name: 'dotnet-library-config', ext: 'dll.config'],
+
+  [name: 'dotnet-executable', ext: 'exe'],
+  [name: 'dotnet-executable-config', ext: 'exe.config'],
+
   [name: 'dotnet-module', ext: 'netmodule'],
-  [name: 'dotnet-exe.config', ext: 'exe.config'],
+  
   [name: 'dotnet-asp', ext: 'dll'],
-  [name: 'dotnet-winexe', ext: 'exe'],
   [name: 'dotnet-gac', ext: 'dll'],
   [name: 'dotnet-gac_generic', ext: 'dll'],
   [name: 'dotnet-gac_msil', ext: 'dll'],
@@ -12,6 +20,23 @@ List types = [
   [name: 'dotnet-nar', ext: 'nar'],
   [name: 'dotnet-netplugin', ext: 'dll'],
   [name: 'dotnet-visual-studio-addin', ext: 'dll'],
+
+  [name: 'dotnet-symbols', ext: 'pdb'],
+  [name: 'ole-type-library', ext: 'tlb'],
+
+  //deprecated 
+  [name: 'library', ext: 'dll'],
+  [name: 'winexe', ext: 'exe'],
+  [name: 'exe.config', ext: 'exe.config'],
+  [name: 'module', ext: 'netmodule'],
+  [name: 'asp', ext: 'dll'],
+  [name: 'gac', ext: 'dll'],
+  [name: 'gac_generic', ext: 'dll'],
+  [name: 'gac_msil', ext: 'dll'],
+  [name: 'gac_32', ext: 'dll'],
+  [name: 'nar', ext: 'nar'],
+  [name: 'netplugin', ext: 'dll'],
+  [name: 'visual-studio-addin', ext: 'dll'],
 ]
 
 // generate META-INF/plexus/components.xml
@@ -34,6 +59,18 @@ new File(plexus, 'components.xml').withWriter('UTF-8') { writer ->
 						type typeDef.name
 					}
 				}
+				
+				// empty-mapping for lifecycle without concrete artifacts
+				component{
+			      role 'org.apache.maven.lifecycle.mapping.LifecycleMapping'
+			      'role-hint' 'dotnet-none'
+			      implementation 'org.apache.maven.lifecycle.mapping.DefaultLifecycleMapping'
+			      configuration {
+			        phases {
+			          
+					}
+			      }
+				}	
 			}
 		}
 	}
